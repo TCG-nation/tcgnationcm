a41db261b719485b2658d14cd935ad029a5ab880
TCG-nation <mikey.sonnenschein@gmail.com>
2025-09-15 07:25:37 +0200

    Update login.tsx

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,3 +1,3 @@
 import dynamic from 'next/dynamic'
-const AuthUI = dynamic(() => import('../parts/AuthUI'), { ssr: false })
+const AuthUI = dynamic(() => import('./parts/AuthUI'), { ssr: false })
 export default function Login() { return <AuthUI /> }
