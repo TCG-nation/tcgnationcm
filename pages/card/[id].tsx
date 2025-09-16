d5867afcd988bac185fcf4aa168a696ed1452e5f
TCG-nation <mikey.sonnenschein@gmail.com>
2025-09-16 17:34:28 +0200

    Update [id].tsx

--- pages/card/[id].tsx
+++ pages/card/[id].tsx
@@ -1,3 +1,8 @@
 import dynamic from 'next/dynamic'
+
+// Laad de client-variant zonder SSR
 const CardDetailClient = dynamic(() => import('../parts/CardDetailClient'), { ssr: false })
-export default function CardDetail() { return <CardDetailClient /> }
+
+export default function CardDetail() {
+  return <CardDetailClient />
+}
