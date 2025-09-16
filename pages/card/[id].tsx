1065411ee8d20b81eaed5a9a2518f69fd89da4e1
TCG-nation <mikey.sonnenschein@gmail.com>
2025-09-16 20:10:22 +0200

    Update [id].tsx

--- pages/card/[id].tsx
+++ pages/card/[id].tsx
@@ -1,13 +1,8 @@
-import { useRouter } from 'next/router'
+import dynamic from 'next/dynamic'
 
-export default function CardDetailTest() {
-  const router = useRouter()
-  const { id } = router.query
-  return (
-    <div style={{padding:24}}>
-      <a href="/dashboard" style={{fontSize:12}}>&larr; Terug</a>
-      <h1 style={{marginTop:8}}>Card detail test</h1>
-      <p>Dit is de testpagina voor kaart met id: <b>{String(id || '')}</b></p>
-    </div>
-  )
+// Laad de client-variant zonder SSR
+const CardDetailClient = dynamic(() => import('../parts/CardDetailClient'), { ssr: false })
+
+export default function CardDetail() {
+  return <CardDetailClient />
 }
